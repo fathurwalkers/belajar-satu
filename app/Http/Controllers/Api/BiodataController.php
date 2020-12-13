217408b941944984d8ad33a3f3194714a6cc6990
fathurwalkers <fathurwalkers44@gmail.com>
2020-12-13 17:45:52 +0800

    first commit

---
+++ app/Http/Controllers/Api/BiodataController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Faker\Factory as Faker;
+use Illuminate\Http\Request;
+
+class BiodataController extends Controller
+{
+    public function generateData()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return view('dashboard.index');
+    }
+}
